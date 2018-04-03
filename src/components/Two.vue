3211df8e52a18981560b2a7c12760594a874cd90
finelai <finelai@ymail.com>
2018-04-03 17:38:21 +0300

    add comments

--- src/components/Two.vue
+++ src/components/Two.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="two">
-    <span>npm</span>
+    <h1>Задание №2 Взаимодействие с сервером по протоколу websocket</h1>
+
+    <div class="comments">
+      <div class="comments__one-comment" v-for="(comment, index) in comments" :key="index">
+        <span>{{ comment.body }}</span>
+        <button @click="deleteComment(comment.id)">Удалить</button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,43 +16,81 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      comments: [
+        {
+          id: `0${Math.floor(Math.random() * 10) + 1}`,
+          body: 'Тестовый коммент 1',
+        },
+        {
+          id: `1${Math.floor(Math.random() * 10) + 1}`,
+          body: 'Это шедевр',
+        },
+        {
+          id: `2${Math.floor(Math.random() * 10) + 1}`,
+          body: 'Это прекрасно',
+        },
+        {
+          id: `3${Math.floor(Math.random() * 10) + 1}`,
+          body: 'Лучшее, что я видел',
+        },
+        {
+          id: `4${Math.floor(Math.random() * 10) + 1}`,
+          body: 'Два чая этому автору',
+        },
+        {
+          id: `5${Math.floor(Math.random() * 10) + 1}`,
+          body: 'Великолепно',
+        },
+        {
+          id: `6${Math.floor(Math.random() * 10) + 1}`,
+          body: 'Мне это нравится',
+        },
+        {
+          id: `7${Math.floor(Math.random() * 10) + 1}`,
+          body: 'Вот это да!',
+        },
+        {
+          id: `8${Math.floor(Math.random() * 10) + 1}`,
+          body: 'У меня нет слов',
+        },
+        {
+          id: `9${Math.floor(Math.random() * 10) + 1}`,
+          body: 'Сильно',
+        },
+      ],
     }
-  }
+  },
+  methods: {
+    deleteComment(num) {
+      console.log(num);
+      for (let n = 0; n < this.comments.length; n++) {
+        console.log(this.comments[n].id);
+        if (this.comments[n].id === num) {
+          this.comments.splice(n, 1);
+          console.log(this.comments);
+        }
+      }
+    },
+  },
 }
 </script>
 
-<style lang="scss">
-* {
-  box-sizing: border-box;
-}
-body {
-  padding: 0;
-  margin: 0;
-}
-#app {
-  font-family: Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-  margin-top: 20px;
-}
+<style lang="scss" scoped>
+  h1 {
+    text-align: center;
+  }
+  .comments {
+    max-width: 320px;
+    margin: 0 auto;
+    
+    &__one-comment {
+      display: flex;
+      justify-content: space-between;
+      padding: 5px;
 
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
+      &:nth-child(odd) {
+        background-color: #f6f6f6;
+      }
+    }
+  }
 </style>
